a378c6703695087db3d5d32ad4e038f860bbbbc4
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-02-14 00:20:11 +0000

    criada a tela de cursos

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+def index(request):
+    courses = Course.objects.all()
+    
+    context = {
+        'courses': courses
+    }
+    
+    return render(request, 'index.html', context)
