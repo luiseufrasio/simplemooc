97bab86426a8016d9f4f1989d146feeb0194e5d3
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-03-07 12:07:31 +0000

    design alterado

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import login
 
+from . import views
+
 app_name='accounts'
 urlpatterns = [
 	path('entrar/', login, {'template_name': 'login.html'}, name='login'),
+	path('cadastre-se/', views.register, name='register'),
 ]
