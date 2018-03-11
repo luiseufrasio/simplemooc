b17ec4adc59fdd1b2a170e9a098d2216c274f3c1
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-03-11 23:57:43 +0000

    painel do usuário criado

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import (login, logout)
 
 from . import views
 
 app_name='accounts'
 urlpatterns = [
+	path('', views.dashboard, name='dashboard'),
 	path('entrar/', login, {'template_name': 'login.html'}, name='login'),
 	path('cadastre-se/', views.register, name='register'),
+	path('sair/', logout, {'next_page': 'core:home'}, name='logout')
 ]
