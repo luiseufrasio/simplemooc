ceac10c7326419eac7669f91fbdf60cf30d95edd
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-03-14 22:42:26 +0000

    alterando a senha do user

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 	path('entrar/', login, {'template_name': 'login.html'}, name='login'),
 	path('sair/', logout, {'next_page': 'core:home'}, name='logout'),
 	path('cadastre-se/', views.register, name='register'),
-	path('editar/', views.edit, name='edit')
+	path('editar/', views.edit, name='edit'),
+	path('editar-senha/', views.edit_password, name='edit_password')
 ]
