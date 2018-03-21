f8d967eed4edcc6a18cf4c54929c55301f74111d
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-03-21 17:40:46 +0000

    O user pode alterar a sua senha através de envio de e-mail.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 	path('entrar/', login, {'template_name': 'login.html'}, name='login'),
 	path('sair/', logout, {'next_page': 'core:home'}, name='logout'),
 	path('cadastre-se/', views.register, name='register'),
+	path('nova-senha/', views.password_reset, name='password_reset'),
+	path('confirmar-nova-senha/<slug:key>/', views.password_reset_confirm, name='password_reset_confirm'),
 	path('editar/', views.edit, name='edit'),
 	path('editar-senha/', views.edit_password, name='edit_password')
 ]
