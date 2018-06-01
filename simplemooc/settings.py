da56625e0f4acd6cd2ff4071c0b82a1a6c0d2462
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-06-01 23:00:44 +0000

    gravando o anúncio juntamente com o disparo (signal) do e-mail

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -133,7 +133,7 @@ DEFAULT_FROM_EMAIL = 'Claris <ana.claris@gmail.com>'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ana.claris@gmail.com'
-EMAIL_HOST_PASSWORD = 'senha'
+EMAIL_HOST_PASSWORD = '1Sapoti6'
 EMAIL_PORT = 587
 
 CONTACT_EMAIL='luiseufrasio@gmail.com'
