532fe9fb4849030abbd7aeb3f2bd396a9ca911f5
Luis Eufrasio <luis.eufrasio@gmail.com>
2018-07-21 18:23:08 +0000

    alterando a resposta via ajax

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,9 @@
+import json
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View, ListView, DetailView
 from django.contrib import messages
+from django.http import HttpResponse
 
 from .models import Thread, Reply
 from .forms import ReplyForm
@@ -71,11 +74,19 @@ class ReplyCorrectView(View):
     correct = True
     
     def get(self, request, pk):
-        reply = get_object_or_404(Reply, pk=pk, author=request.user)
+        reply = get_object_or_404(Reply, pk=pk, thread__author=request.user)
         reply.correct = self.correct
         reply.save()
-        messages.success(request, 'Resposta atualizada com sucesso')
-        return redirect(reply.thread.get_absolute_url())
+        message = 'Resposta atualizada com sucesso'
+        if request.is_ajax():
+            data = {
+                'success': True,
+                'message': message
+            }
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        else:
+            messages.success(request, message)
+            return redirect(reply.thread.get_absolute_url())
 
 index = ForumView.as_view()
 thread = ThreadView.as_view()
